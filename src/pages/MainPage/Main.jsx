eb0eb1dade0f605415b10a3fe8396ab63727a126
Suliman <sssuliman@bk.ru>
2022-09-21 21:53:06 +0300

    Простой набросок mainPage

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import styles from "./main.module.scss";
 
 const Main = () => {
-    return (
-        <div>
-            
+  return (
+    <>
+      <section className={styles.banner}>
+        <div className={styles.main_img}>
+          <div className={styles.text_content}>
+            <h3>Do you like to eat tasty and healthy?</h3>
+            <h1>Don't wait !</h1>
+            <h4>Find the right and healthy way to eat with us</h4>
+            <button>View diets</button>
+          </div>
         </div>
-    );
+        <div className={styles.block}></div>
+      </section>
+      <section className={styles.blocks}>
+        
+      </section>
+    </>
+  );
 };
 
 export default Main;
