9140597625c9fe32c46731021fb75782b65fd8a8
Suliman <sssuliman@bk.ru>
2022-09-22 10:56:07 +0300

    Сделал переходы на странице Body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./body.module.scss";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [gender, setGender] = useState("male");
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.sviper}>
+        <Link to={"/body/boy"}>male</Link>
+        <Link to={"girl"}>female</Link>
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default Body;
