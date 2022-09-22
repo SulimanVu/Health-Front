cc87af369c579426ebc1a9a6d81651ec262ce352
Suliman <sssuliman@bk.ru>
2022-09-22 13:59:48 +0300

    Подправил mainPage

--- src/pages/MainPage/Main.jsx
+++ src/pages/MainPage/Main.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchDiet } from "../../features/dietSlice";
 import styles from "./main.module.scss";
 
 const Main = () => {
+  const diet = useSelector((state) => state.diet.diet);
+  console.log(diet);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchDiet());
+  }, [dispatch]);
   return (
     <>
-    <section className={styles.banner}>
-      <div className={styles.main_img}>
-        <div className={styles.text_content}>
-          <h3>Do you like to eat tasty and healthy?</h3>
-          <h1>Don't wait !</h1>
-          <h4>Find the right and healthy way to eat with us</h4>
-          <button>View diets</button>
+      <section className={styles.banner}>
+        <div className={styles.main_img}>
+          <div className={styles.text_content}>
+            <h3>Do you like to eat tasty and healthy?</h3>
+            <h1>Don't wait !</h1>
+            <h4>Find the right and healthy way to eat with us</h4>
+            <button><Link to={"/#diet"}>View diets</Link></button>
+          </div>
         </div>
-      </div>
-      <div className={styles.block}></div>
-    </section>
-    <section className={styles.blocks}>
-      
-    </section>
-  </>
+        <div className={styles.block}></div>
+      </section>
+      <section className={styles.diet_blocks} id={'diet'}>
+        {diet.map((item, index) => {
+          return (
+            <div key={index}>
+              <h1>{item.name}</h1>
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
 };
 
