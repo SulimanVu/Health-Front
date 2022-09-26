54828fac4decfcd24143d46c0ec7284f9f10bf2a
Ashwash <latar14@bk.ru>
2022-09-26 11:19:09 +0300

    Новые исправления на странице Diet

--- src/pages/Diet/Diet.jsx
+++ src/pages/Diet/Diet.jsx
@@ -1,36 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchDiet } from '../../features/dietSlice';
-import { useSelector } from 'react-redux/es/exports';
-import img from '../../public/grecha.jpg'
-import styles from '../../pages/Diet/Diet.module.scss'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchDiet } from "../../features/dietSlice";
+import { useSelector } from "react-redux/es/exports";
+import styles from "../../pages/Diet/Diet.module.scss";
 const Diet = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const diets = useSelector((state) => state.diet.diet);
 
-    const diets = useSelector((state) => state.diet.diet)
+  useEffect(() => {
+    dispatch(fetchDiet());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(fetchDiet());
-    }, [dispatch])
-
-    return (
-        <div className={styles.cardsbody}>
-            <div className={styles.card}>
-                {diets.map((item) => {
-                    return (
-                        <div>
-                            <h2>
-                                {item.name}
-                            </h2>
-                            <p>{item.description}</p>
-                            <div className={styles.image}><img src={img} alt="" /></div>
-                        </div>
-                    )
-                })}
+  return (
+    <div className={styles.cardsbody}>
+      {diets.map((item) => {
+        return (
+          <div className={styles.cards}>
+            <div className={styles.image}>
+              <img src={`http://localhost:3010/images/${item.image}`} alt="#" />
             </div>
-        </div>
-    );
+            <div className={styles.cardtext}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Diet;
