bf76a07f06acf020d5d13d0a6d3c2468d8742df1
Ashwash <latar14@bk.ru>
2022-09-26 14:12:06 +0300

    Сделал прелоадер на странице Diet

--- src/pages/Diet/Diet.jsx
+++ src/pages/Diet/Diet.jsx
@@ -3,31 +3,52 @@ import { useDispatch } from "react-redux";
 import { fetchDiet } from "../../features/dietSlice";
 import { useSelector } from "react-redux/es/exports";
 import styles from "../../pages/Diet/Diet.module.scss";
+import { Dna } from "react-loader-spinner";
+
 const Diet = () => {
   const dispatch = useDispatch();
 
   const diets = useSelector((state) => state.diet.diet);
+  const loader = useSelector((state) => state.diet.loader);
 
   useEffect(() => {
     dispatch(fetchDiet());
   }, [dispatch]);
 
   return (
-    <div className={styles.cardsbody}>
-      {diets.map((item) => {
-        return (
-          <div className={styles.cards}>
-            <div className={styles.image}>
-              <img src={`http://localhost:3010/images/${item.image}`} alt="#" />
-            </div>
-            <div className={styles.cardtext}>
-              <h2>{item.name}</h2>
-              <p>{item.description}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      {loader ? (
+        <div className={styles.loaders}>
+          <Dna
+            visible={true}
+            height="300"
+            width="300"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      ) : (
+        <div className={styles.cardsbody}>
+          {diets.map((item) => {
+            return (
+              <div className={styles.cards}>
+                <div className={styles.image}>
+                  <img
+                    src={`http://localhost:3010/images/${item.image}`}
+                    alt="#"
+                  />
+                </div>
+                <div className={styles.cardtext}>
+                  <h2>{item.name}</h2>
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
